faca715df6614c7870dba5eb4e18bbf71b630235
Jake Lee <jakeogtgg@gmail.com>
2022-04-03 16:24:46 +0900

    as and attrs

--- src/App.js
+++ src/App.js
@@ -4,31 +4,33 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+  padding: 10px 20px;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50%;
+const Input = styled.input.attrs({ type: "search", required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="lightgrey" />
+    <Father as="header">
+      <Input></Input>
+      <Input></Input>
+      <Input></Input>
+      <Input></Input>
+      <Input></Input>
+      <Input></Input>
+      <Btn>Logging</Btn>
+      <Btn as="a" href="/">
+        Logging
+      </Btn>
     </Father>
   );
 }
-// function App() {
-//   return (
-//     <div style={{display: "flex"}}>
-//       <div style={{backgroundColor: "teal", width: 100, height: 100}}></div>
-//       <div style={{backgroundColor: "tomato", width: 100, height: 100}}></div>
-//     </div>
-//   );
-// }
 
 export default App;
