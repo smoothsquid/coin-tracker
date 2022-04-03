cbcb45da6d6de71c6e48ae4c6b1eecb5147a80c6
Jake Lee <jakeogtgg@gmail.com>
2022-04-03 23:19:54 +0900

    add typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="tomato" />
+      <Circle bgColor="teal" />
+    </div>
+  );
+}
+
+export default App;
