d6c09f83c6a108a37fec16443b330a2ed4cdd85b
Jake Lee <jakeogtgg@gmail.com>
2022-04-03 16:38:14 +0900

    animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
 import styled from "styled-components";
+import { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-  padding: 10px 20px;
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform: rotate(180deg); 
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  } 
 `;
 
-const Input = styled.input.attrs({ type: "search", required: true })`
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  animation: ${rotateAnimation} 1s linear infinite;
+
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 72px;
+    }
+    &:active {
+      opacity: 0.5;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-      <Btn>Logging</Btn>
-      <Btn as="a" href="/">
-        Logging
-      </Btn>
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🥰</span>
+      </Box>
+    </Wrapper>
   );
 }
 
