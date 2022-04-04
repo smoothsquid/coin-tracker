25a40b0bb9597940d1a5c421f48e9370589242bc
Jake Lee <jakeogtgg@gmail.com>
2022-04-04 21:07:24 +0900

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import Router from "./Router";
 
 function App() {
-  return (
-    <Container>
-      <Title>Welcome</Title>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
